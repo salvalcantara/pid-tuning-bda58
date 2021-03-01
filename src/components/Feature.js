505035d0f07a7529ad72cb82f9a7475e90531aa0
Stackbit Code Editor <support@stackbit.com>
2021-03-01 08:17:14 +0000

    Feature.js: changed

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -16,7 +16,6 @@ export default class Feature extends React.Component {
         let media_pos = _.get(feature, 'media_position', null) || 'top';
         let is_horiz = false;
         let is_vert = false;
-        let blueish
         if ((((_.get(feature, 'title', null) || _.get(feature, 'subtitle', null)) || _.get(feature, 'content', null)) || _.get(feature, 'actions', null))) {
              has_text = true;
         }
