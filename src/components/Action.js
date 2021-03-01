2a73c947b195a61527a7c4d51fad5a6ef2f51048
Stackbit Code Editor <support@stackbit.com>
2021-03-01 08:25:27 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -13,7 +13,7 @@ export default class Action extends React.Component {
             <Link to={withPrefix(_.get(action, 'url', null))}
             	{...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
             	{...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-            	className={classNames('blueish', {'btn': ((action_style === 'primary') || (action_style === 'secondary')) || _.get(action, 'has_icon', null), 'btn--primary': action_style === 'primary', 'btn--secondary': action_style === 'secondary', 'btn--icon': _.get(action, 'has_icon', null) && (action_icon_pos === 'center'), 'btn--clear': _.get(action, 'has_icon', null) && (action_style === 'link')})}>
+            	className={classNames({'btn': ((action_style === 'primary') || (action_style === 'secondary')) || _.get(action, 'has_icon', null), 'btn--primary': action_style === 'primary', 'btn--secondary': action_style === 'secondary', 'btn--icon': _.get(action, 'has_icon', null) && (action_icon_pos === 'center'), 'btn--clear': _.get(action, 'has_icon', null) && (action_style === 'link')})}>
             	{_.get(action, 'has_icon', null) ? (<React.Fragment>
             		<Icon {...this.props} icon={_.get(action, 'icon', null)} />
             		<span className={classNames({'order-first': action_icon_pos === 'right', 'sr-only': action_icon_pos === 'center'})}>{_.get(action, 'label', null)}</span>
