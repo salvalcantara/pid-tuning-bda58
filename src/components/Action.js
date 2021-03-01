dbfa58ca178e040b235ac579a379d74e0f7b2cb6
Stackbit Code Editor <support@stackbit.com>
2021-03-01 08:26:01 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -12,7 +12,7 @@ export default class Action extends React.Component {
         return (
             <Link to={withPrefix(_.get(action, 'url', null))}
             	{...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-            	{...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}, 'blueish') : null)}
+            	{...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
             	className={classNames({'btn': ((action_style === 'primary') || (action_style === 'secondary')) || _.get(action, 'has_icon', null), 'btn--primary': action_style === 'primary', 'btn--secondary': action_style === 'secondary', 'btn--icon': _.get(action, 'has_icon', null) && (action_icon_pos === 'center'), 'btn--clear': _.get(action, 'has_icon', null) && (action_style === 'link')})}>
             	{_.get(action, 'has_icon', null) ? (<React.Fragment>
             		<Icon {...this.props} icon={_.get(action, 'icon', null)} />
